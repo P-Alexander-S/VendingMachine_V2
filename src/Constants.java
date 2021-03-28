10efce1a41d9d41ebae4c9ca41c21b1621ee8070
P-Alexander-S <alexander.p_r@mail.ru>
2021-03-28 13:18:55 +0300

    Commit

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+
+    public static final String TIME_REGEX = "(([01]\\d)|(2[0-3])):((0\\d)|([0-5]\\d))";
+    public static final String DATE_REGEX = "(0\\d|[12]\\d|31)-(0[1-9]|1[012])-\\d\\d\\d\\d";
+}
+
+
